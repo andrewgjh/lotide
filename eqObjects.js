c4438e66de83e671dd9f3a6caf86970616834bbc
Andrew Ho <andrew.gjh@gmail.com>
2022-01-16 18:55:37 -0500

    Refactored eqObjects to allow for object values

--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,4 @@
-
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -8,27 +7,103 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function (arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
   return (arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index])
 };
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  return eqArrays(Object.keys(object1).flat().sort(), Object.keys(object2).flat().sort()) 
-  && eqArrays(Object.values(object1).flat().sort(),Object.values(object2).flat().sort());  
+const eqObjects = function (object1, object2) {
+  const keyArray1 = Object.keys(object1).sort();
+  const keyArray2 = Object.keys(object2).sort();
+  const sameKey = eqArrays(keyArray1, keyArray2);
+  if (!sameKey) {
+    return false;
+  }
+  for (let key of keyArray1) {
+    const value1 = object1[key];
+    const value2 = object2[key]
+    if (value1 === value2 || eqArrays(value1, value2) || eqObjects(value1, value2)) {
+      continue;
+    }
+    return false;
+  }
+  return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+const ab = {
+  a: "1",
+  b: "2"
+};
+const ba = {
+  b: "2",
+  a: "1"
+};
 console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
+const abc = {
+  a: "1",
+  b: "2",
+  c: "3"
+};
 console.log(eqObjects(ab, abc)); // => false
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
+const cd = {
+  c: "1",
+  d: ["2", 3]
+};
+const dc = {
+  d: ["2", 3],
+  c: "1"
+};
 console.log(eqObjects(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd2 = {
+  c: "1",
+  d: ["2", 3, 4]
+};
 console.log(eqObjects(cd, cd2)); // => false
+
+
+
+console.log(eqObjects({
+  a: {
+    z: 1
+  },
+  b: 2
+}, {
+  a: {
+    z: 1
+  },
+  b: 2
+})); // => true
+
+console.log(eqObjects({
+  a: {
+    y: 0,
+    z: 1
+  },
+  b: 2
+}, {
+  a: {
+    z: 1
+  },
+  b: 2
+})); // => false
+console.log(eqObjects({
+  a: {
+    y: 0,
+    z: 1
+  },
+  b: 2
+}, {
+  a: 1,
+  b: 2
+})); // => false
+
+
+
+module.exports = eqObjects;
