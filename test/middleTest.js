1aa6f87de7ca821efe19ca9aecf872340fc243e2
Andrew Ho <andrew.gjh@gmail.com>
2022-01-17 10:05:16 -0500

    refactored tests to use mocha&chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,36 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", function () {
+  it("Returns empty array when passes in one element", function () {
+    let input = [1];
+    let expected = [];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("Returns empty array when passes in two element", function () {
+    let input = [1, 2];
+    let expected = [];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("Returns [2] when passes in [1,2,3]", function () {
+    let input = [1, 2, 3];
+    let expected = [2];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("Returns [2,3] when passes in [1,2,3,4]", function () {
+    let input = [1, 2, 3, 4];
+    let expected = [2, 3];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("Returns [3] when passes in [1,2,3,4,5]", function () {
+    let input = [1, 2, 3, 4, 5];
+    let expected = [3];
+    assert.deepEqual(middle(input), expected);
+  });
+  it("Returns [3,4] when passes in [1,2,3,4,5,6]", function () {
+    let input = [1, 2, 3, 4, 5, 6];
+    let expected = [3, 4];
+    assert.deepEqual(middle(input), expected);
+  });
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,22 @@
 const tail= require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(words.length, 3);
-assertEqual(tail(words).length, 2);
+
+describe("#tailFunction", function(){
+  it("returns ['Lighthouse', 'Labs'] when passed in ['Yo Yo', 'Lighthouse', 'Labs']", function(){
+    input = ["Yo Yo", "Lighthouse", "Labs"];
+    expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(tail(input), expected);
+  });
+  it("returns '[2,3,4,5]' when passed in [1,2,3,4,5]", function(){
+    input = [1,2,3,4,5];
+    expected = [2,3,4,5];
+    assert.deepEqual(tail(input), expected);
+  });
+  it("returns empty array when passed in 1 element]", function(){
+    input = ["Don't Delete Me!"];
+    expected = [];
+    assert.deepEqual(tail(input), expected);
+  });
+
+})
