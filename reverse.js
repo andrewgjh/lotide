a278bfb59f93dd5c9e5df37b3afd09944132e402
Andrew Ho <andrew.gjh@gmail.com>
2022-01-17 15:21:19 -0500

    Exported Reverse Function

--- reverse.js
+++ reverse.js
@@ -11,27 +11,4 @@ const reverse = function(array) {
   return reversedString;
 }
 
-// const reverseWord = function (string) {
-//   let splitUp = string.split("");
-//   let placeHolder = "";
-//   let finalWord = "";
-//   for (let i = 0; i < string.length / 2; i++) {
-//     placeHolder = splitUp[i];
-//     splitUp[i] = splitUp[string.length - i - 1];
-//     splitUp[string.length - i - 1] = placeHolder;
-//   }
-//   for (let char of splitUp) {
-//     finalWord += char;
-//   }
-//   return finalWord;
-// };
-
-// const reverse = function (array) {
-//   let reversed = "";
-//   for (let word of array) {
-//     reversed += reverseWord(word) + " ";
-//   }
-//   return reversed;
-// };
-
-console.log(reverse(args));
+module.exports= reverse;
